7d442e248ef531b072333c36cdd1e871f8004b60
Cody Nichoson <codynichoson@gmail.com>
2022-02-17 21:01:15 -0600

    Noise and slip working (I think)

--- nusim/src/nusim.cpp
+++ nusim/src/nusim.cpp
@@ -114,8 +114,8 @@ void wheel_cmd_callback(const nuturtlebot_msgs::WheelCommands &wheelcmd)
     wheelvel.right = wheel_cmd_noise(get_random()) * wheelcmd.right_velocity * motor_cmd_to_radsec;
     
     // calculate encoder stuff to populate sensor_data
-    int encoder_left = (int)(((wheelvel.left/rate) + wheelangles.left)/encoder_ticks_to_rad);
-    int encoder_right = (int)(((wheelvel.right/rate) + wheelangles.right)/encoder_ticks_to_rad);
+    int encoder_left = (int)(((wheelvel.left/rate) + slip_wheelangles.left)/encoder_ticks_to_rad);
+    int encoder_right = (int)(((wheelvel.right/rate) + slip_wheelangles.right)/encoder_ticks_to_rad);
 
     sensor_data.left_encoder = encoder_left;
     sensor_data.right_encoder = encoder_right;
@@ -243,15 +243,15 @@ int main(int argc, char * argv[])
         walls_pub.publish(wall_arr);
 
         // Generate a gaussian variable:
-        std::uniform_real_distribution<> left_noise(0, 2); // (mean, variance)
-        std::uniform_real_distribution<> right_noise(0, 2); // (mean, variance)
+        std::uniform_real_distribution<> left_noise(1, 2); // (mean, variance)
+        std::uniform_real_distribution<> right_noise(1, 2); // (mean, variance)
 
         double left_slip = left_noise(get_random())*wheelvel.left/rate;
         double right_slip = right_noise(get_random())*wheelvel.right/rate;
             
         wheelangles = {.left = ((wheelvel.left/rate)+wheelangles.left), .right = ((wheelvel.right/rate)+wheelangles.right)};
+        new_config = ddrive.fKin(wheelangles, new_config);
         slip_wheelangles = {.left = slip_wheelangles.left + left_slip, .right = slip_wheelangles.right + right_slip};
-        new_config = ddrive.fKin(slip_wheelangles, new_config);
 
         // populate transform and publish
         transformStamped.header.stamp = ros::Time::now();
