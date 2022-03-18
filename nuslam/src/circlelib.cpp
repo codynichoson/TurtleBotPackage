d16ca9f23bb6f302afb93464d4a37bfd97d60d56
Cody Nichoson <codynichoson@gmail.com>
2022-03-18 11:54:17 -0500

    Tuned mean angle and std deviation

--- nuslam/src/circlelib.cpp
+++ nuslam/src/circlelib.cpp
@@ -164,7 +164,7 @@ namespace nuslam
             double stdev = sqrt(val_sum / (angle_list.size() - 1.0));
             ROS_WARN("stdev: %f", stdev);
 
-            if (turtlelib::rad2deg(mean) > 85.0 && turtlelib::rad2deg(mean) < 140.0 && stdev < 0.35)    // if cluster meets circle characteristics
+            if (turtlelib::rad2deg(mean) > 100.0 && turtlelib::rad2deg(mean) < 150.0 && stdev < 0.35)    // if cluster meets circle characteristics
             {
                 new_cluster_list.push_back(cluster_list.at(i));   // keep it
             }
